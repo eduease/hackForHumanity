d319b76573fc51d1b52ae2b464921fa09f3ae997
VIVEK RAJARAM <vivraj17@gmail.com>
2019-03-02 15:27:06 -0800

    Update in flow
    Chnages in Index.js
    and added new pug files

--- EduEase/routes/index.js
+++ EduEase/routes/index.js
@@ -4,7 +4,56 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'EduEase' });
+});
+
+router.post('/sl', (req, res, next) => {
+
+    return res.render('studentLogin', { title: 'EduEase' });
+
+});
+
+router.post('/ss', (req, res, next) => {
+
+    return res.render('studentSignup', { title: 'EduEase' });
+
+});
+
+router.post('/tl', (req, res, next) => {
+
+    return res.render('teacherLogin', { title: 'EduEase' });
+
+});
+
+router.post('/ts', (req, res, next) => {
+
+    return res.render('teacherSignup', { title: 'EduEase' });
+
+});
+
+router.post('/SignupTeacher', function (req, res) {
+
+    //TODO
+    res.render('teacherLogin', { title: 'Express Pool' });
+
+});
+
+router.post('/SignupStudent', function (req, res) {
+
+    //TODO
+    res.render('studentLogin', { title: 'Express Pool' });
+
+});
+
+router.post('/loginStudent', function (req, res) {
+    console.log("Inside Login Student")
+    res.render('student', { title: 'Express Pool' });
+
+});
+
+router.post('/loginTeacher', function (req, res) {
+    res.render('teacher', { title: 'Express Pool' });
+
 });
 
 module.exports = router;
